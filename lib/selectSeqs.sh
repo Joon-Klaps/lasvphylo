2aca127d3b6fefc946690a177c8b3a0e52e2a20d
Joon-Klaps <joon.klaps@kuleuven.be>
2025-12-04 16:29:08 +0100

    Update other files

--- lib/selectSeqs.sh
+++
@@ -1,7 +0,0 @@
-#! bin/bash
-cp $0 input.fa;
-grep ">" $1 | sed "s/>//g" > patterns.txt;
-awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' input.fa > tmp;
-grep -Ff patterns.txt tmp > tmp2;
-tr "\t" "\n" tmp2 > $0;
-rm input.fa tmp tmp2;
